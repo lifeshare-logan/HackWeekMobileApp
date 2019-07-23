58d970b2e96a4342032003916c464a6c0b7f5278
crowleaj <alex.crowley@lifesharetech.com>
2019-07-23 14:44:20 -0400

    Fix merge conflicts

--- user-home.js
+++ user-home.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View, Button} from 'react-native';
-import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+import { Content } from 'native-base';
 import EventCard from './components/event-card';
 import { StackNavigator } from 'react-navigation';
 
 export default class UserHome extends Component {
     render() {
       const { navigate } = this.props.navigation;
-      
+
       let activities = [
         {title: "Foo", time: "10:00AM"},
         {title: "Bar", time: "11:00AM"},
